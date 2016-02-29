af2302e5e6ce02243e2ca826ce25bbd7a77ae019
Kevin Amorim <kevinamorim4@gmail.com>
2016-02-29 17:55:14 +0000

    Switching between sub-menus, temporary and lame

---
+++ Assets/Scripts/Menus/Upgrades/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Menus.Upgrades {
+
+	public class ButtonManager : MonoBehaviour {
+		
+		Camera mainCamera;
+		
+		// Use this for initialization
+		void Start () {
+			//mainCamera = Camera.main;
+			//mainCamera.transform.position = new Vector3 (0f, 1000f, 0f);
+		}
+	
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		
+		
+	}
+}
+
