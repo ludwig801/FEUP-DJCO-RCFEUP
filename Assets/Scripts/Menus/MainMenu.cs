bf544739577f86b22a2f41a91a3d636028f8ac96
ee09063 <ee09063@fe.up.pt>
2016-03-02 16:07:02 +0000

    Credits Menu + Checkpoint Model

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -6,34 +6,68 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	public Canvas quitMenu;
+	public Canvas creditsMenu;
+	public Canvas mainMenu;
+
 	public Button playButton;
 	public Button quitButton;
+	public Button creditsButton;
 
 	// Use this for initialization
 	void Start () {
 		quitMenu = quitMenu.GetComponent<Canvas> ();
+		creditsMenu = creditsMenu.GetComponent<Canvas> ();
+		mainMenu = mainMenu.GetComponent<Canvas> ();
+
 		playButton = playButton.GetComponent<Button> ();
 		quitButton = quitButton.GetComponent<Button> ();
+		creditsButton = creditsButton.GetComponent<Button> ();
+
 		quitMenu.enabled = false;
+		creditsMenu.enabled = false;
 	}
 	
-	public void ExitPress(){
+	public void PressQuitAtMainMenu(){
 		quitMenu.enabled = true;
+		creditsMenu.enabled = false;
+
 		playButton.enabled = false;
 		quitButton.enabled = false;
+		creditsButton.enabled = false;
 	}
 
-	public void NoPress(){
+	public void PressCreditsAtMainMenu(){
 		quitMenu.enabled = false;
+		creditsMenu.enabled = true;
+
+		playButton.enabled = false;
+		quitButton.enabled = false;
+		creditsButton.enabled = false;
+	}
+
+	public void PressBackAtCreditsMenu(){
+		quitMenu.enabled = false;
+		creditsMenu.enabled = false;
+
 		playButton.enabled = true;
 		quitButton.enabled = true;
+		creditsButton.enabled = true;
 	}
 
-	public void StartLevel(){
+	public void PressNoAtQuitMenu(){
+		quitMenu.enabled = false;
+		creditsMenu.enabled = false;
+
+		playButton.enabled = true;
+		quitButton.enabled = true;
+		creditsButton.enabled = true;
+	}
+
+	public void PressStart(){
 		SceneManager.LoadScene ("MainMenu");
 	}
 
-	public void QuitGame(){
+	public void PressYesAtQuitMenu(){
 		Application.Quit();
 	}
 }
