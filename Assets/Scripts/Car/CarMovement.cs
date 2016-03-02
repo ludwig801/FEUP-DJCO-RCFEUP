f70b0aff78dd69c3d29138536095282bd55de8ec
Luís Magalhães <lmaga801@outlook.com>
2016-03-02 00:09:24 +0000

    Implemented cool car movement

--- Assets/Scripts/Car/CarMovement.cs
+++ Assets/Scripts/Car/CarMovement.cs
@@ -9,21 +9,50 @@ public class CarMovement : MonoBehaviour
     public float Acceleration;
     public float TopSpeed;
     public float AngularAcceleration;
+    public float HandbrakeStrength;
     public float MaxSteeringAngle;
-    public float MaxBodyInclination;
+    public float TurnThreshold;
+    public float MaxBodySideAngle;
+    public float MaxBodyAccelAngle;
+    public float MaxBodyBrakeAngle;
     public Transform BodyPivot;
-    public Text Velocity;
+    public Text VelocityText;
 
     [SerializeField]
     Rigidbody _rigidbody;
     [SerializeField]
     float _topVelocity;
+    [SerializeField]
+    bool _flying;
+    [SerializeField]
+    bool _movingForward;
+
+    Vector3 Velocity
+    {
+        get
+        {
+            return _rigidbody.velocity;
+        }
+
+        set
+        {
+            _rigidbody.velocity = value;
+        }
+    }
 
     float SpeedKMH
     {
         get
         {
-            return UnitConverter.VelocityToKmh(_rigidbody.velocity.magnitude);
+            return UnitConverter.VelocityToKmh(Velocity.magnitude);
+        }
+    }
+
+    float Speed
+    {
+        get
+        {
+            return Velocity.magnitude;
         }
     }
 
@@ -34,7 +63,7 @@ public class CarMovement : MonoBehaviour
 
     void Update()
     {
-        Velocity.text = string.Format("Velocity: {0:0}", _rigidbody.velocity.magnitude);
+        VelocityText.text = string.Format("Velocity: {0:0}", Speed);
         _topVelocity = UnitConverter.KmhToVelocity(TopSpeed);
     }
 
@@ -58,17 +87,42 @@ public class CarMovement : MonoBehaviour
         steering = Input.GetAxisRaw("Horizontal");
         handbrake = Mathf.Clamp(handbrake, 0, 1);
 
-        _rigidbody.AddForce(throttle * transform.forward * Acceleration);
+        _movingForward = Vector3.Dot(transform.forward, Velocity) >= 0;
 
-        if (Vector3.Dot(transform.forward, _rigidbody.velocity) < 0)
-            _rigidbody.AddTorque(-steering * transform.up * AngularAcceleration);
-        else
-            _rigidbody.AddTorque(steering * transform.up * AngularAcceleration);
-       
+        ApplyDrive(throttle, handbrake);
+        ApplySteering(steering);
 
-        _rigidbody.angularVelocity = Vector3.ClampMagnitude(_rigidbody.angularVelocity, 2f);
-        _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, _topVelocity);
+        UpdateWheels(steering);
+        UpdateBody(throttle, steering);
 
+        ClampRotation();
+    }
+
+    void ApplyDrive(float throttle, float handbrake)
+    {
+        if (!_flying)
+        {
+            var realForward = new Vector3(transform.forward.x, 0, transform.forward.z);
+            var vectorReference = throttle >= 0 ?
+                (_movingForward ? realForward : -Velocity.normalized) :
+                (_movingForward ? Velocity.normalized : realForward);
+
+            _rigidbody.AddForce(throttle * vectorReference * Acceleration * _rigidbody.mass);
+        }
+
+        Velocity = Vector3.ClampMagnitude(Velocity, _topVelocity);
+    }
+
+    void ApplySteering(float steering)
+    {
+        _rigidbody.AddTorque((_movingForward ? 1 : -1) * (_flying ? 0.25f : 1) * steering * transform.up * AngularAcceleration * _rigidbody.mass);
+
+        var turnClamp = Mathf.Clamp01(Speed / TurnThreshold);
+        _rigidbody.angularVelocity = Vector3.ClampMagnitude(_rigidbody.angularVelocity, turnClamp);
+    }
+
+    void UpdateWheels(float steering)
+    {
         foreach (var axle in Axles)
         {
             if (axle.Steering)
@@ -77,8 +131,36 @@ public class CarMovement : MonoBehaviour
                 axle.RightWheel.localRotation = Quaternion.Lerp(axle.RightWheel.localRotation, Quaternion.Euler(steering * MaxSteeringAngle, 0, 0), Time.fixedDeltaTime * 5f);
             }
         }
+    }
 
-        BodyPivot.localRotation = Quaternion.Lerp(BodyPivot.localRotation, Quaternion.Euler(MaxBodyInclination * -throttle, BodyPivot.localRotation.y, MaxBodyInclination * steering), Time.fixedDeltaTime * 5f);
+    void UpdateBody(float throttle, float steering)
+    {
+        var sideRotation = MaxBodySideAngle * steering;
+        var forwardRotation = throttle >= 0 ? throttle * -MaxBodyAccelAngle : throttle * -MaxBodyBrakeAngle;
+        BodyPivot.localRotation = Quaternion.Lerp(BodyPivot.localRotation, Quaternion.Euler(forwardRotation, BodyPivot.localRotation.y, sideRotation), Time.fixedDeltaTime * 5f);
+    }
+
+    void ClampRotation()
+    {
+        var rotation = transform.localRotation.eulerAngles;
+        rotation.x = Utils.ClampRotation(rotation.x, 45);
+        rotation.z = Utils.ClampRotation(rotation.z, 10);
+        transform.localRotation = Quaternion.Euler(rotation);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        _flying = false;
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        _flying = false;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        _flying = true;
     }
 }
 
---
+++ Assets/Scripts/Libraries/Utils.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class Utils
+{
+    public static float ClampRotation(float value, float spread)
+    {
+        if (value <= 180)
+            return Mathf.Min(value, spread);
+        else
+            return Mathf.Max(value, 360 - spread);
+    }
+}
