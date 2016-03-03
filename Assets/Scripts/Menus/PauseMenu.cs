6c8e45fc3499e4079bc7b3e35c7e63720bab4c74
ee09063 <ee09063@fe.up.pt>
2016-03-03 18:31:10 +0000

    Pause Menu added to GameScene

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public RectTransform pauseMenu;
+
+	public Button continueButton;
+	public Button quitButton;
+
+	private bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		continueButton = continueButton.GetComponent<Button> ();
+		quitButton = quitButton.GetComponent<Button> ();
+
+		pauseMenu.gameObject.SetActive (false);
+		isPaused = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (isPaused) {
+				isPaused = false;
+				Time.timeScale = 1;
+				pauseMenu.gameObject.SetActive (false);
+			} else {
+				isPaused = true;
+				Time.timeScale = 0;
+				pauseMenu.gameObject.SetActive (true);
+			}
+		}
+	}
+
+	public void pressContinue(){
+		if (isPaused) {
+			isPaused = false;
+			Time.timeScale = 1;
+			pauseMenu.gameObject.SetActive (false);
+		}
+	}
+
+	public void pressQuit(){
+		Time.timeScale = 1;
+		SceneManager.LoadScene ("MainMenu");
+	}
+}
