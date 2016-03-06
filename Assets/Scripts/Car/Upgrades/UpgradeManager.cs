35641643518838a12c121e1243802823cc679d9f
Kevin Amorim <kevinamorim4@gmail.com>
2016-03-06 19:43:17 +0000

    Created upgrade manager script

--- Assets/Scripts/Car/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Car/Upgrades/UpgradeManager.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class UpgradeManager : MonoBehaviour
 {
 
-    public Car Car;
-    private ICollection<Upgrade> _upgrades;
-
-    public void Save()
+    public void UpgradeSpeed()
     {
+        Debug.Log("SPEED+++");
+    }
+
+    public void UpgradeHandling()
+    {
+        Debug.Log("HANDLING+++");
+    }
+
+    public void UpgradeWeight()
+    {
+        Debug.Log("WEIGHT+++");
     }
 
 }
