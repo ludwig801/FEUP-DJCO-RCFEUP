9b8ab3014cfeaf961ce65cd42d550568ba24864f
Kevin Amorim <kevinamorim4@gmail.com>
2016-03-06 22:00:57 +0000

    UpgradesReader

--- Assets/Scripts/Car/Upgrades/UpgradeReader.cs
+++ Assets/Scripts/Car/Upgrades/UpgradeReader.cs
@@ -1,12 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Xml;
+using UnityEngine;
 
-public static class UpgradeReader  {
+public static class UpgradeReader
+{
 
     private static string Filename = "Upgrades.sav";
 
     public static int GetUpgradeLevel(int upgradeId)
     {
-        return 0;
+        var xml = new XmlDocument();
+
+        xml.Load(Filename);
+
+        var upgradesNode = xml.SelectSingleNode("/Upgrades").ChildNodes;
+
+        foreach (XmlNode upgrade in upgradesNode)
+        {
+            if (int.Parse(upgrade.Attributes[0].Value) == upgradeId)
+            {
+                return int.Parse(upgrade.Attributes[1].Value);
+            }
+        }
+
+        return -1;
     }
 
 }
