b6763d5a18fe76555f0ecb2564b4bb110b816329
Luís Magalhães <lmaga801@outlook.com>
2016-03-25 22:58:32 +0000

    Created Selection Behavior

--- Assets/Scripts/Menus/MainMenu/NewRaceValues.cs
+++ Assets/Scripts/Menus/MainMenu/NewRaceValues.cs
@@ -1,17 +1,55 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class NewRaceValues : MonoBehaviour
 {
-    public Button[] 
+    public RectTransform NumPlayers, NumLaps;
+    public List<ButtonValue> NumPlayersBtns, NumLapsBtns;
+    public int Players, Laps;
+    public Color ColorSelected, ColorDefault;
 
     void Start()
     {
+        foreach (var btn in NumPlayersBtns)
+        {
+            btn.Btn.image.color = ColorDefault;
+            btn.Btn.onClick.AddListener(() =>
+            {
+                SelectNumPlayers(btn);
+            });
+        }
 
+        SelectNumPlayers(NumPlayersBtns[0]);
+        SelectNumLaps(NumLapsBtns[0]);
     }
 
-    void Update()
+    private void SelectNumPlayers(ButtonValue bv)
     {
+        bv.Btn.image.color = ColorSelected;
+        foreach (var item in NumPlayersBtns)
+        {
+            if (item != bv)
+                item.Btn.image.color = ColorDefault;
+        }
+        Players = bv.Value;
+    }
 
+    private void SelectNumLaps(ButtonValue bv)
+    {
+        bv.Btn.image.color = ColorSelected;
+        foreach (var item in NumLapsBtns)
+        {
+            if (item != bv)
+                item.Btn.image.color = ColorDefault;
+        }
+        Laps = bv.Value;
+    }
+
+    [System.Serializable]
+    public class ButtonValue
+    {
+        public Button Btn;
+        public int Value;
     }
 }
