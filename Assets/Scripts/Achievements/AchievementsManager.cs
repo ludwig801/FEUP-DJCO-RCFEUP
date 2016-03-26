f71619bc1d7c2d56188a00909b5691e5d79ba854
Kevin Amorim <kevinamorim4@gmail.com>
2016-03-26 15:21:39 +0000

    Working on achievements manager

--- Assets/Scripts/Achievements/AchievementsManager.cs
+++ Assets/Scripts/Achievements/AchievementsManager.cs
@@ -1,16 +1,59 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class AchievementsManager : MonoBehaviour {
 
-    public static string[] AchievementsDescriptions = { "Drive." };
+    private bool[] _achievementsStatus = new bool[AchievementsInfo.GetTotalAchievementsCount()];
+
+    public Car Car;
 
 	void Start () {
-	
+	    
+        for(var i = 0; i < _achievementsStatus.Length; i++)
+        {
+            _achievementsStatus[i] = AchievementsIO.GetAchievementStatus(i) == 1;
+        }
+
 	}
 
 	void Update () {
 
+        for(var i = 0; i < _achievementsStatus.Length; i++)
+        {
+            if(!_achievementsStatus[i])
+            {
+                if(VerifyAchievementWithId(i))
+                {
+                    AchievementsIO.ChangeAchievementStatus(i);
+                }
+            }
+        }
 
-	
 	}
+
+    private bool VerifyAchievementWithId(int id)
+    {
+        switch(id)
+        {
+            case 0:
+                var currentVelocity = Car.CarMovement.Rigidbody.velocity.magnitude;
+                var topSpeed = Car.CarMovement.SpeedStatsKMH.CurrentTopSpeed;
+
+                Debug.Log("Velocity: " + currentVelocity);
+                Debug.Log("Top Speed: " + topSpeed);
+
+                if(UnitConverter.VelocityToKMH(currentVelocity) >= topSpeed)
+                {
+                    return true;
+                }
+
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+
+        return false;
+    }
 }
